2d1d9aeed8bbf3e11c69a16f0aa2895e40cc5a5a
Nobuhito SATO <nobuhito.sato@gmail.com>
2018-12-11 00:27:50 +0900

    update icon for ios

--- icon/svg2png.sh
+++ icon/svg2png.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-rsvg-convert icon/logo.svg -h 1024 -w 1024 -f png -o icon/logo.png
-rsvg-convert icon/logo.svg -h 64 -w 64 -f png -o doc/img/logo.64.png
+
+# ios
+rsvg-convert icon/logo.svg -b white -h 1024 -w 1024 -f png -o icon/icon_ios.png
+
+# android
+# rsvg-convert icon/logo_mask.svg -h 1024 -w 1024 -f png -o icon/logo_mask.png
 # convert icon/logo.png -matte icon/icon-mask.png -compose copy-opacity -composite icon/icon.png
+
+# app logo
+# convert icon/logo.png -resize 6.25% doc/img/logo.64.png
+rsvg-convert icon/logo.svg -h 1024 -w 1024 -f png -o icon/logo.png
 convert icon/logo.png -resize 50% icon/icon.512.png
+rsvg-convert icon/logo.svg -h 64 -w 64 -f png -o doc/img/logo.64.png
 
 # flutter pub pub run flutter_launcher_icons:main
